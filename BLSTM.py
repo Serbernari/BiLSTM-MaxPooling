d2de51c010867a2f6b7f3c96f69f531c565bbff9
Serbernari <hirugas7@gmail.com>
2019-02-03 01:01:52 +0700

    Add files via upload

--- BLSTM.py
+++ BLSTM.py
@@ -14,7 +14,7 @@ from keras.utils.np_utils import to_categorical
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import cross_validate   
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import f1_score
 
 from scipy.sparse import csr_matrix
@@ -26,13 +26,13 @@ labels = []
 texts = []
          
 data = pd.read_csv("intents_new.csv",sep = ',', header = None, names = ['type', 'text'])
-data['type'] = data['type'].map({'a': 1, 'b': 2,
-                                 'c':3, 'd':4,
-                                 'e':5,'f':6, 'g':7,
-                                 'h':8,'o':9, 'p':10,
-                                 'q':11, 'w':12, 'e':13 , 'r':14,
-                                 't':15,'y':16,
-                                 'n':17,'m':18})
+data['type'] = data['type'].map({'Как заблокировать сим-карту': 1, 'Верните деньги': 2,
+                                 'Переведите на человека':3, 'Почему вы подключаете услуги без моего согласия?':4,
+                                 'Не приходят смс':5,'Как поменять домашний регион?':6, 'Что за  тарифный план?':7,
+                                 'Как заказать обратный звонок?':8,'Как узнать кодовое слово?':9, 'Позвоните мне!':10,
+                                 'сим карта не работает':11, 'Тариф без интернета':12, 'Что со связью?':13 , 'Не отправляются смс':14,
+                                 'Почему меня заблокировали?':15,'Как разблокировать сим-карту.':16,
+                                 'Как сменить номер':17,'Статус заявки':18})
 data.text=data.text.astype(str)
 classes = np.array(data['type'])
 texts = np.array(data['text'])
@@ -73,57 +73,51 @@ for word, i in word_index.items():
 
 #### Model coustruction ###
 
-model = Sequential()
-model.add(Embedding(num_words,
+seed = 42
+np.random.seed(seed)
+
+F1_data = open('F1_data_file.txt', 'w')
+
+# define 10-fold cross validation test harness
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cvscores = []
+
+for train, test in kfold.split(texts, classes):
+  # create model
+    model = Sequential()
+    model.add(Embedding(num_words,
                     embedding_dim,
                     input_length=max_len, trainable = False, weights = [embedding_matrix]))
-model.add(SpatialDropout1D(0.2))
-model.add(Bidirectional(CuDNNLSTM(64, return_sequences=True)))
-model.add(Bidirectional(CuDNNLSTM(32, return_sequences=True)))
-model.add(Dropout(0.25))
-model.add(GlobalMaxPooling1D())
-model.add(Dense(units=18, activation='softmax'))
-model.summary()
+    model.add(SpatialDropout1D(0.2))
+    model.add(Bidirectional(CuDNNLSTM(64, return_sequences=True)))
+    model.add(Bidirectional(CuDNNLSTM(32, return_sequences=True)))
+    model.add(Dropout(0.25))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(units=18, activation='softmax'))
+    model.summary()
 
-model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam' ,metrics = ['accuracy']) 
+    model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam' ,metrics = ['accuracy']) 
 
-batch_size = 32 #mini batch is preferable
-indices = np.arange(0, len(y_train), 1, dtype=np.int32)
-np.random.shuffle(indices)
-history = model.fit(x_train[indices], y_train[indices]-1, epochs=26, batch_size=batch_size, verbose=1, validation_split=0.1)
+    batch_size = 32 #mini batch is preferable
+    indices = np.arange(0, len(y_train), 1, dtype=np.int32)
+    np.random.shuffle(indices)
+    history = model.fit(x_train[indices], y_train[indices]-1, epochs=26, batch_size=batch_size, verbose=1, validation_split=0.1)
+    
+    ### F1 metrix ###
 
+    class_list = [i for i in range(18)]
+    output = model.predict(x_test)
 
-### F1 metriс ###
+    y_pred = [np.argmax(output[i]) + 1 for i in range(len(output))]
 
-class_list = [i for i in range(18)]
-output = model.predict(x_test)
-#print(output)
-y_pred = [np.argmax(output[i]) + 1 for i in range(len(output))]
-print('pred:',y_pred[:10])
-print('real:',y_test[:10])
-f1_macro = f1_score(y_test, y_pred, labels=class_list, average = 'macro' )
-f1_micro = f1_score(y_test, y_pred, labels=class_list, average = 'micro' )
-print('F1_macro = ',f1_macro)
-print('F1_micro = ',f1_micro)
- 
-
-### Plot drawing ###
-
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper left')
-plt.show()
+    f1_macro = f1_score(y_test, y_pred, labels=class_list, average = 'macro' )
+    f1_micro = f1_score(y_test, y_pred, labels=class_list, average = 'micro' )
+    print('F1_macro = ',f1_macro)
+    print('F1_micro = ',f1_micro)
+    F1_data.write(str(f1_macro))
+    F1_data.write("\n")
+   
+F1_data.close()
 
 ### Model saving ###
 
